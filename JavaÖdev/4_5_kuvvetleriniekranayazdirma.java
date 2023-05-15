d081a25a5bb134984c9a7d0b708de4122a659437
İZEL <100135761+izzuysl@users.noreply.github.com>
2023-05-15 18:28:03 +0300

    Create 4_5_kuvvetleriniekranayazdirma.java

---
+++ JavaÖdev/4_5_kuvvetleriniekranayazdirma.java
@@ -0,0 +1,20 @@
+public class sayilar {
+    public static void main(String[] args) {
+        int n;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("bir sayı giriniz:");
+        n = inp.nextInt();
+        System.out.println("4'ün kuvvetleri");
+
+        for(int i = 1; i<=n; i*=4){
+            System.out.println(i);
+        }
+        System.out.println("------------");
+        System.out.println("5'in kuvvetleri");
+
+
+        for(int i = 1; i<=n; i*=5){
+            System.out.print(i);
+        }
+    }
+}
